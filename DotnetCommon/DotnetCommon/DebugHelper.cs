8a14dd67cb455e22e5ee738ad9b699258b7d8d4f
kresimir.budor <kresimir.budor@heksadev.com>
2022-04-12 15:50:45 +0200

    DebugHelper

---
+++ DotnetCommon/DotnetCommon/DebugHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotnetCommon
+{
+    public static class DebugHelper
+    {
+        public static bool IsDebug
+        {
+            get
+            {
+#pragma warning disable
+#if DEBUG
+                return true;
+#endif
+                return false;
+#pragma warning restore
+            }
+        }
+    }
+}
